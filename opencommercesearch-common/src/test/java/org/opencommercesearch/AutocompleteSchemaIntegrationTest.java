b84ef7b9b9cf31f41249dbf9bce7fff3fb7dc036
Ricardo Merizalde <rmerizalde@backcountry.com>
2013-01-11 02:24:46 -0700

    Added a few char filter for autocomplete. Need to find a better way to do this.
    Sticking to regular expression for the moment instead of rewriting a new tokenizer

--- opencommercesearch-common/src/test/java/org/opencommercesearch/AutocompleteSchemaIntegrationTest.java
+++ opencommercesearch-common/src/test/java/org/opencommercesearch/AutocompleteSchemaIntegrationTest.java
@@ -5,10 +5,12 @@ import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
 import org.apache.solr.client.solrj.request.FieldAnalysisRequest;
 import org.apache.solr.client.solrj.response.AnalysisResponseBase;
 import org.apache.solr.client.solrj.response.FieldAnalysisResponse;
-import org.junit.Before;
+import org.apache.solr.common.util.NamedList;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
+import java.util.List;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Handler;
 import java.util.logging.Level;
@@ -16,10 +18,13 @@ import java.util.logging.Level;
 /**
  * @author rmerizalde
  */
+@Category(IntegrationSearchTest.class)
 public class AutocompleteSchemaIntegrationTest extends SolrTestCaseJ4 {
 
+    private static EmbeddedSolrServer solrServer;
+
     @BeforeClass
-    public static void setupSuite() {
+    public static void setupSuite() throws Exception {
         Handler[] handlers = java.util.logging.Logger.getLogger("").getHandlers();
         ConsoleHandler consoleHandler = null;
         for (Handler handler : handlers) {
@@ -29,26 +34,174 @@ public class AutocompleteSchemaIntegrationTest extends SolrTestCaseJ4 {
           }
         }
         consoleHandler.setLevel(Level.SEVERE);
+        initCore("solrconfig.xml", "schema.xml", "solr", "autocomplete");
+        solrServer = new EmbeddedSolrServer(h.getCore());
     }
 
-    @Before
-    public void setupTest() throws Exception {
-        initCore("solrconfig.xml", "schema.xml", "solr", "autocomplete");
-    }
 
     @Test
     public void testLowercase() throws Exception {
-        EmbeddedSolrServer solrServer = new EmbeddedSolrServer(h.getCore());
         FieldAnalysisRequest request = new FieldAnalysisRequest();
 
         request.addFieldType("normalizedQuery");
         request.setFieldValue("  MY search TERM  ");
-        FieldAnalysisResponse response = request.process(solrServer);
-        FieldAnalysisResponse.Analysis analysis = response.getFieldTypeAnalysis("normalizedQuery");
 
-        assertEquals("my search term", extractTextWords(analysis));
+        //FieldAnalysisRequest request = new FieldAnalysisRequest();
+        //FieldAnalysisResponse response = request.process(solrServer);
+        //FieldAnalysisResponse.Analysis analysis = response.getFieldTypeAnalysis("normalizedQuery");
+        assertEquals("my search term", extractTextWords(solrServer.request(request)));
     }
 
+    @Test
+    public void testPunctuation() throws Exception {
+        FieldAnalysisRequest request = new FieldAnalysisRequest();
+
+        request.addFieldType("normalizedQuery");
+        request.setFieldValue(" foo. bar t.baz b.a.z. ... foo.. 2.5 bar.");
+        assertEquals("foo bar t.baz b.a.z. foo 2.5 bar", extractTextWords(solrServer.request(request)));
+    }
+
+    @Test
+    public void testCommas() throws Exception {
+        FieldAnalysisRequest request = new FieldAnalysisRequest();
+
+        request.addFieldType("normalizedQuery");
+        request.setFieldValue("foo,bar,, baz , bar bar,");
+        assertEquals("foo bar baz bar bar", extractTextWords(solrServer.request(request)));
+    }
+
+    @Test
+    public void testDoubleQuotes() throws Exception {
+        FieldAnalysisRequest request = new FieldAnalysisRequest();
+
+        request.addFieldType("normalizedQuery");
+        request.setFieldValue("\"jacket\"");
+        assertEquals("jacket", extractTextWords(solrServer.request(request)));
+
+        request.setFieldValue("26\"");
+        assertEquals("26\"", extractTextWords(solrServer.request(request)));
+
+        request.setFieldValue("2.5  \"");
+        assertEquals("2.5 \"", extractTextWords(solrServer.request(request)));
+
+        request.setFieldValue("jacket \"red\"");
+        assertEquals("jacket red", extractTextWords(solrServer.request(request)));
+
+        request.setFieldValue("jacket \"\"\"red\"\"\"");
+        assertEquals("jacket red", extractTextWords(solrServer.request(request)));
+    }
+
+    @Test
+    public void testSingleQuotes() throws Exception {
+        FieldAnalysisRequest request = new FieldAnalysisRequest();
+
+        request.addFieldType("normalizedQuery");
+        request.setFieldValue("kid's");
+        assertEquals("kid's", extractTextWords(solrServer.request(request)));
+
+        request.setFieldValue("kids'");
+        assertEquals("kids", extractTextWords(solrServer.request(request)));
+
+        request.setFieldValue("'jacket'");
+        assertEquals("jacket", extractTextWords(solrServer.request(request)));
+
+        request.setFieldValue("red''' ");
+        assertEquals("red", extractTextWords(solrServer.request(request)));
+
+        request.setFieldValue(" '''black''' ");
+        assertEquals("black", extractTextWords(solrServer.request(request)));
+    }
+
+    @Test
+    public void testMinus() throws Exception {
+        FieldAnalysisRequest request = new FieldAnalysisRequest();
+
+        request.addFieldType("normalizedQuery");
+        request.setFieldValue("-40 ");
+        assertEquals("-40", extractTextWords(solrServer.request(request)));
+
+        request.setFieldValue("--40");
+        assertEquals("-40", extractTextWords(solrServer.request(request)));
+
+        request.setFieldValue("foo - bar");
+        assertEquals("foo bar", extractTextWords(solrServer.request(request)));
+
+        request.setFieldValue("foo -- bar");
+        assertEquals("foo bar", extractTextWords(solrServer.request(request)));
+
+        request.setFieldValue("foo--");
+        assertEquals("foo-", extractTextWords(solrServer.request(request)));
+    }
+
+    @Test
+    public void testAmpersandAndPercentage() throws Exception {
+        FieldAnalysisRequest request = new FieldAnalysisRequest();
+
+        request.addFieldType("normalizedQuery");
+        request.setFieldValue("foo&bar");
+        assertEquals("foo&bar", extractTextWords(solrServer.request(request)));
+
+        request.setFieldValue("foo  & bar");
+        assertEquals("foo & bar", extractTextWords(solrServer.request(request)));
+
+        request.setFieldValue("40%");
+        assertEquals("40%", extractTextWords(solrServer.request(request)));
+
+        request.setFieldValue("40 %");
+        assertEquals("40 %", extractTextWords(solrServer.request(request)));
+    }
+
+    @Test
+    public void testNonAscii() throws Exception {
+        // the schema currently support only 3 non-ascii characters ë ø ö
+        FieldAnalysisRequest request = new FieldAnalysisRequest();
+
+        request.addFieldType("normalizedQuery");
+        request.setFieldValue("(#foo: bar(baz_foo)_ ) ");
+        assertEquals("foo bar baz foo", extractTextWords(solrServer.request(request)));
+    }
+
+    @Test
+    public void testRemoveOtherSpecialChars() throws Exception {
+        // the schema currently support only 3 non-ascii characters ë ø ö
+        FieldAnalysisRequest request = new FieldAnalysisRequest();
+
+        request.addFieldType("normalizedQuery");
+        request.setFieldValue("  lolë  norrøna völkl  ");
+        assertEquals("lolë norrøna völkl", extractTextWords(solrServer.request(request)));
+    }
+
+    /**
+     * Helper method to extract tokens from the last phase of the analysis
+     *
+     * Using weak type approach due to bug SOLR-2834
+     *
+     * https://issues.apache.org/jira/browse/SOLR-2834
+     */
+    private String extractTextWords(NamedList<Object> response) {
+        NamedList<NamedList<NamedList<NamedList<Object>>>> analyis = (NamedList<NamedList<NamedList<NamedList<Object>>>>) response.get("analysis");
+        NamedList<Object> phases = (NamedList<Object>) analyis.get("field_types").get("normalizedQuery").get("index");
+        List<NamedList<Object>> tokenInfos = (List<NamedList<Object>>) phases.getVal(phases.size() - 1);
+        StringBuffer textWords = new StringBuffer();
+
+        for (NamedList<Object> tokenInfo : tokenInfos) {
+            String type = (String) tokenInfo.get("type");
+
+            if ("word".equals(type)) {
+                textWords.append(tokenInfo.get("text")).append(' ');;
+            }
+        }
+
+        if (textWords.length() > 0) {
+            textWords.setLength(textWords.length() - 1);
+        }
+        return textWords.toString();
+    }
+
+
+    /**
+     * Helper method to extract tokens from the last phase of the analysis
+     */
     private String extractTextWords(FieldAnalysisResponse.Analysis analysis) {
         StringBuffer textWords = new StringBuffer();
         AnalysisResponseBase.AnalysisPhase lastPhase = null;
@@ -59,7 +212,7 @@ public class AutocompleteSchemaIntegrationTest extends SolrTestCaseJ4 {
 
         for (AnalysisResponseBase.TokenInfo tokenInfo : lastPhase.getTokens()) {
             if ("word".equals(tokenInfo.getType())) {
-                textWords.append(tokenInfo.getText()).append(" ");
+                textWords.append(tokenInfo.getText()).append(' ');
             }
         }
 
@@ -68,5 +221,4 @@ public class AutocompleteSchemaIntegrationTest extends SolrTestCaseJ4 {
         }
         return textWords.toString();
     }
-
 }
