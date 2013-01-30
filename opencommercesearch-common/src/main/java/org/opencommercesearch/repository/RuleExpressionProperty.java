eaa01d7f74844c6857bbc67274f83a61449c21ef
Gregorio Segura <gsegura@backcountry.com>
2013-01-30 15:00:48 -0600

    Extend Product Catalog to add support for Rule Category Pages

---
+++ opencommercesearch-common/src/main/java/org/opencommercesearch/repository/RuleExpressionProperty.java
@@ -0,0 +1,11 @@
+package org.opencommercesearch.repository;
+
+public class RuleExpressionProperty {
+
+	public static final String ITEM_DESCRIPTOR = "ruleExpression";
+	public static final String VALUE = "value";
+	public static final String NESTED_LEVEL = "nestLevel";
+	public static final String TYPE = "type";
+	public static final String OPERATOR = "operator";
+}
+
