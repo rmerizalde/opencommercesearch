070c79552bd71694957f0dd81b07aeb8a0d5366d
Ricardo Merizalde <rmerizalde@backcountry.com>
2013-03-16 15:46:18 -0600

    Made loadData method protected

--- opencommercesearch-common/src/main/java/org/opencommercesearch/service/SequentialDataLoaderService.java
+++ opencommercesearch-common/src/main/java/org/opencommercesearch/service/SequentialDataLoaderService.java
@@ -272,7 +272,7 @@ public abstract class SequentialDataLoaderService<K extends Comparable, V> exten
      * @param sqlQuery the SQL to be executed
      * @param recordProcessor the processor to process each record
      */
-    private void loadData(String sqlQuery, RecordProcessor<V> recordProcessor) {
+    protected void loadData(String sqlQuery, RecordProcessor<V> recordProcessor) {
         Connection connection = null;
 
         try {
