bc77914747d0d6363d9f02bd17756e08a9a3fbb9
Javier Mendez <jmendez@backcountry.com>
2013-10-08 17:29:31 -0600

    Fixed rule feeed issues when there are missing fields

--- opencommercesearch-common/src/main/java/org/opencommercesearch/feed/RuleFeed.java
+++ opencommercesearch-common/src/main/java/org/opencommercesearch/feed/RuleFeed.java
@@ -47,6 +47,8 @@ import static org.opencommercesearch.repository.RankingRuleProperty.ATTRIBUTE;
  */
 public class RuleFeed extends BaseRestFeed {
 
+    public static String[] REQUIRED_FIELDS = { "id", "category" };
+
     private Map<String, String> strengthMap;
     private RulesBuilder rulesBuilder;
 
@@ -142,10 +144,17 @@ public class RuleFeed extends BaseRestFeed {
             }
             catch(RepositoryException e) {
                 //Can't load categories
+                if(isLoggingError()) {
+                    logError("Can't load categories for rule " + rule.getRepositoryId(), e);
+                }
             }
 
-            if (ruleJsonObj.get(RuleConstants.FIELD_CATEGORY) == null) {
-                ruleJsonObj.put(RuleConstants.FIELD_CATEGORY, "null");
+            if (ruleJsonObj.get(RuleConstants.FIELD_CATEGORY) == null || ((JSONArray) ruleJsonObj.get(RuleConstants.FIELD_CATEGORY)).isEmpty()) {
+                //Don't index this rule, it has no category information.
+                if(isLoggingWarning()) {
+                    logWarning("Rule " + rule.getRepositoryId() + " has no valid category data associated to it.");
+                    return null;
+                }
             }
         }
         else {
@@ -193,7 +202,7 @@ public class RuleFeed extends BaseRestFeed {
 
     @Override
     protected String[] getRequiredItemFields() {
-        return null;
+        return REQUIRED_FIELDS;
     }
 
     /**
