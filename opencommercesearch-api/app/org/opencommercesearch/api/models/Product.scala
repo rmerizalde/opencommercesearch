c8ee19f38d2419b8ef58cf55435603db515620bb
Javier Mendez <jmendez@backcountry.com>
2013-10-25 15:21:51 -0600

    Fix isOutlet field always set to false

--- opencommercesearch-api/app/org/opencommercesearch/api/models/Product.scala
+++ opencommercesearch-api/app/org/opencommercesearch/api/models/Product.scala
@@ -222,7 +222,7 @@ case class ProductList(products: Seq[Product], feedTimestamp: Long) {
             doc.setField("isRetail", isRetail)
             doc.setField("skuCount", skuCount)
             doc.setField("isCloseout", isCloseout)
-            doc.setField("isOutlet", sku.isOutlet)
+            doc.setField("isOutlet", sku.isOutlet.get)
             for (isPastSeason <- sku.isPastSeason) { doc.setField("isPastSeason", isPastSeason) }
             if (gender != null) { doc.setField("gender", gender ) }
 
