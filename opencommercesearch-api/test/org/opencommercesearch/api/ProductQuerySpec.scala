34885ded5359a479bfde720e0f7c24f77cc133bf
Brian Bergendahl <bbergendahl@backcountry.com>
2016-06-15 20:56:53 -0600

    Allow backorderable skus with inventory to show in results

--- opencommercesearch-api/test/org/opencommercesearch/api/ProductQuerySpec.scala
+++ opencommercesearch-api/test/org/opencommercesearch/api/ProductQuerySpec.scala
@@ -16,11 +16,22 @@ class ProductQuerySpec extends Specification with Mockito {
   private val lang = new Lang("en", "US")
 
   private def setupSortBy(sortParam: String, expectedSortField: String) = {
+    implicit val request = FakeRequest(GET, "/test?sort="+sortParam)
     implicit val context = Context(true, lang)
-	implicit val request = FakeRequest(GET, "/test?sort="+sortParam)
-	val productQuery = new ProductQuery("query", "site")
-	productQuery.withSorting
-	productQuery.getSortField() must beEqualTo(expectedSortField)
+    val productQuery = new ProductQuery("query", "site")
+    productQuery.withSorting
+    productQuery.getSortField() must beEqualTo(expectedSortField)
+  }
+
+  private def setupExcludeBackorder(site: String, shouldFilterBackorder: Boolean) = {
+    implicit val context = Context(true, lang)
+    implicit val request = FakeRequest(GET, "/test")
+    val query = new ProductQuery("query", site)
+    if (shouldFilterBackorder) {
+      query.getFilterQueries.toList should contain ("(allowBackorderUS:false OR stockLevelUS:[1 TO *])")
+    } else {
+      query.getFilterQueries.toList should not contain ("allowBackorderUS:false OR stockLevelUS:[1 TO *]")
+    }
   }
 
   "ProductQuery" should {
@@ -34,6 +45,17 @@ class ProductQuerySpec extends Specification with Mockito {
        setupSortBy("bestSeller asc", "sellRanksite asc" )
      }
     }
+    "Filter allowBackorder products only when the stock level is 0 when site has backorder excluded" in {
+       running(new FakeApplication(additionalConfiguration = Map("sites.excludeBackorder" -> "siteWithExcludedBackorder"))) {
+         setupExcludeBackorder("siteWithExcludedBackorder", true)
+       }
+    }
+    "Not Filter allowBackorder products for a site that has backorder enabled" in {
+       running(new FakeApplication(additionalConfiguration = Map("sites.excludeBackorder" -> "siteWithExcludedBackorder"))) {
+         setupExcludeBackorder("siteAllowBackorder", false)
+       }
+    }
+
   }
 
   "ProductQuery" should {
